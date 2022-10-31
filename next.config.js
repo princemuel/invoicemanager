b91f787b84b0b3cbb9df18fdb05abfbbc6c98d89
princemuel <vansomecsam@gmail.com>
2022-10-31 03:47:22 +0100

    new commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- types/models.ts
+++ types/models.ts
@@ -2,4 +2,32 @@
           DATA MODELS
  *===============================*
 */
-export {};
+export type IInvoices = Array<IInvoice>;
+
+export interface IInvoice {
+  id: string;
+  createdAt: string;
+  paymentDue: string;
+  description: string;
+  paymentTerms: number;
+  clientName: string;
+  clientEmail: string;
+  status: string;
+  senderAddress: IAddress;
+  clientAddress: IAddress;
+  items: IProduct[];
+  total: number;
+}
+
+interface IAddress {
+  street: string;
+  city: string;
+  postCode: string;
+  country: string;
+}
+interface IProduct {
+  name: string;
+  quantity: number;
+  price: number;
+  total: number;
+}
