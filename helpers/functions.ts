08440db5ec78d3988d5d203b993226e92cd48345
Prince Muel <vansomecsam@gmail.com>
2022-12-17 01:44:25 +0100

    chore: update repo files and project dependencies
    [Ticket: X]

--- helpers/functions.ts
+++ helpers/functions.ts
@@ -47,3 +47,9 @@ const invoice: Invoice = {
 };
 
 isPaidInvoice(invoice);
+
+export const trim = (string: string) => string?.trim();
+
+export const capitalize = (string: string) => {
+  return string[0].toUpperCase() + string.slice(1);
+};
