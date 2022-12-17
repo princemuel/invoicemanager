daadcbb2327ff8862a1c328981678f74f551f03e
Prince Muel <vansomecsam@gmail.com>
2022-12-17 01:58:08 +0100

    fix: resolve build issue for successful deployment

--- helpers/functions.ts
+++ helpers/functions.ts
@@ -47,9 +47,3 @@ const invoice: Invoice = {
 };
 
 isPaidInvoice(invoice);
-
-export const trim = (string: string) => string?.trim();
-
-export const capitalize = (string: string) => {
-  return string[0].toUpperCase() + string.slice(1);
-};
