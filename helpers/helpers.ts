4e1da573dba82fc1f30683be82b9ccaed6c22575
Prince Muel <vansomecsam@gmail.com>
2022-12-19 04:34:44 +0100

    feat: add more utility types and functions
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ helpers/helpers.ts
@@ -0,0 +1,55 @@
+// NOTE: To be refactored
+export const shippingFee = (amount: number) => {
+  const fee = amount * 0.01;
+  return convertFee(fee);
+};
+
+export const vatFee = (amount: number) => {
+  const fee = amount * 0.001;
+  return convertFee(fee);
+};
+
+export const grandTotal = (total: number, shipping: number, vat: number) => {
+  const fee = total + shipping + vat;
+  return convertFee(fee);
+};
+
+export const formatPrice = (price?: number) => {
+  if (typeof price !== 'number')
+    throw new Error("THe item's price must be a number");
+
+  return Intl.NumberFormat('en-GB', {
+    maximumFractionDigits: 2,
+    style: 'currency',
+    currency: 'GBP',
+  }).format(price);
+};
+
+export function joinDate(
+  date: number | Date | undefined,
+  formatOptions: Intl.DateTimeFormatOptions[],
+  separator: string
+) {
+  function callback(options: Intl.DateTimeFormatOptions | undefined) {
+    let dateFormatter = new Intl.DateTimeFormat('en', options);
+    return dateFormatter.format(date);
+  }
+  return formatOptions?.map(callback).join(separator);
+}
+
+export const formatDate = (date?: string) => {
+  if (typeof date !== 'string')
+    throw new Error("The date must be of type 'string'");
+
+  let formatOptions: Intl.DateTimeFormatOptions[] = [
+    { day: 'numeric' },
+    { month: 'short' },
+    { year: 'numeric' },
+  ];
+
+  return joinDate(new Date(date), formatOptions, ' ');
+};
+
+const convertFee = (fee: number) => {
+  return parseInt(fee.toFixed(0));
+};
--- helpers/index.ts
+++ helpers/index.ts
@@ -1,4 +1,4 @@
 export * from './constants';
-export * from './fees';
 export * from './functions';
+export * from './helpers';
 export * from './utils';
