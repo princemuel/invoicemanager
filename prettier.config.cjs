8ad9c83d5990c4137f9c13a9d702c6c8627e7db9
Prince Muel <vansomecsam@gmail.com>
2023-01-09 06:24:14 +0100

    ci: add prettier config
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ prettier.config.cjs
@@ -0,0 +1,16 @@
+// prettier.config.cjs
+module.exports = {
+  tailwindConfig: "./tailwind.config.js",
+
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
