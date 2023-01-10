3adb66bb928eb7ce8ff3648acdd65539f324f599
Prince Muel <vansomecsam@gmail.com>
2023-01-10 19:20:27 +0100

    feat(env): add env variable constants
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const constants = {
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET || "erhjebrgjhetbgethj",
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || "rkjjrebgjhbgj4hgb",
+  JWT_ACCESS_EXPIRATION: process.env.JWT_ACCESS_EXPIRATION || "5 minutes",
+  JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION || "7 days",
+};
