574fd981002801ade7ac697e7696320867e195eb
Prince Muel <vansomecsam@gmail.com>
2023-01-10 19:25:45 +0100

    ci: setup nexus graphql types configuration
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ gql/index.ts
@@ -0,0 +1,21 @@
+import { connectionPlugin, makeSchema } from "nexus";
+import * as path from "path";
+import * as typeDefs from "./models";
+
+export const schema = makeSchema({
+  types: typeDefs,
+  plugins: [connectionPlugin()],
+  outputs: {
+    typegen: path.join(process.cwd(), "generated", "nexus-typegen.ts"),
+    schema: path.join(process.cwd(), "generated", "schema.gql"),
+  },
+  contextType: {
+    export: "Context",
+    module: path.join(process.cwd(), "gql", "context.ts"),
+  },
+  nonNullDefaults: {
+    output: true,
+  },
+});
+
+export * from "./context";
