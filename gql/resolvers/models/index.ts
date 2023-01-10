341d8120cc17a2e649b18134e0508ea8474c0ac5
Prince Muel <vansomecsam@gmail.com>
2023-01-10 20:30:06 +0100

    feat: add nexus data models
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ gql/resolvers/models/index.ts
@@ -0,0 +1,3 @@
+export * from "./invoice";
+export * from "./misc";
+export * from "./user";
---
+++ gql/resolvers/models/misc.ts
@@ -0,0 +1,33 @@
+import { DateTimeResolver } from "graphql-scalars";
+import { asNexusMethod, enumType, objectType } from "nexus";
+
+export const DateTime = asNexusMethod(DateTimeResolver, "datetime");
+
+export const Address = objectType({
+  name: "Address",
+  definition(t) {
+    t.string("street");
+    t.string("city");
+    t.string("postCode");
+    t.string("country");
+  },
+});
+export const Role = enumType({
+  name: "Role",
+  members: ["DRAFT", "PENDING", "PAID"],
+});
+
+export const InvoiceItem = objectType({
+  name: "InvoiceItem",
+  definition(t) {
+    t.nonNull.string("name");
+    t.int("quantity");
+    t.float("price");
+    t.float("total");
+  },
+});
+
+export const Status = enumType({
+  name: "Status",
+  members: ["DRAFT", "PENDING", "PAID"],
+});
