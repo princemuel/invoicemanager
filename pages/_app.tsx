a6b1756694a22cb25bda0dd46da228871f1dcd4a
Prince Muel <vansomecsam@gmail.com>
2023-01-22 23:48:45 +0100

    feat: setup state and query client providers
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
-import 'assets/styles/main.css';
-import { Layout } from 'components';
-import { ThemeProvider } from 'next-themes';
-import Head from 'next/head';
-import type { AppPropsWithLayout } from 'types';
+import {
+  DehydratedState,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import "assets/styles/main.css";
+import { Layout } from "components";
+import { ModalProvider } from "context";
+import { queryOptions } from "lib";
+import { ThemeProvider } from "next-themes";
+import Head from "next/head";
+import * as React from "react";
+import type { AppPropsWithLayout } from "types";
+//
+type PageProps = {
+  dehydratedState: DehydratedState;
+};
 
-function App({ Component, pageProps }: AppPropsWithLayout) {
+function App({ Component, pageProps }: AppPropsWithLayout<PageProps>) {
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
+  const [queryClient] = React.useState(() => new QueryClient(queryOptions));
 
   return (
     <>
@@ -13,8 +27,19 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
 
-      <ThemeProvider enableSystem={true} attribute='class'>
-        {getLayout(<Component {...pageProps} />)}
+      <ThemeProvider
+        storageKey='preferred-theme'
+        enableSystem={true}
+        attribute='class'
+      >
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps?.dehydratedState ?? {}}>
+            <ModalProvider>
+              {getLayout(<Component {...pageProps} />)}
+            </ModalProvider>
+            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+          </Hydrate>
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
