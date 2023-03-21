71d4564032c32330697eef11b424c84b0ff9e9a3
Prince Muel <vansomecsam@gmail.com>
2023-03-21 07:55:44 +0100

    chore: remove unused imports
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- common/svg-icons.ts
+++ common/svg-icons.ts
@@ -1,12 +1,11 @@
-import IconArrowDownSVG from 'assets/svgs/icon-arrow-down.svg';
-import IconArrowLeftSVG from 'assets/svgs/icon-arrow-left.svg';
-import IconArrowRightSVG from 'assets/svgs/icon-arrow-right.svg';
-import IconCalendarSVG from 'assets/svgs/icon-calendar.svg';
-import IconCheckSVG from 'assets/svgs/icon-check.svg';
-import IconDeleteSVG from 'assets/svgs/icon-delete.svg';
-import IconAddSVG from 'assets/svgs/icon-plus.svg';
-import IconEmptySVG from 'assets/svgs/illustration-empty.svg';
-import LogoSVG from 'assets/svgs/logo.svg';
+import IconArrowDownSVG from "assets/svgs/icon-arrow-down.svg";
+import IconArrowLeftSVG from "assets/svgs/icon-arrow-left.svg";
+import IconArrowRightSVG from "assets/svgs/icon-arrow-right.svg";
+import IconCalendarSVG from "assets/svgs/icon-calendar.svg";
+import IconCheckSVG from "assets/svgs/icon-check.svg";
+import IconDeleteSVG from "assets/svgs/icon-delete.svg";
+import IconAddSVG from "assets/svgs/icon-plus.svg";
+import LogoSVG from "assets/svgs/logo.svg";
 
 export {
   IconAddSVG,
@@ -16,6 +15,5 @@ export {
   IconCalendarSVG,
   IconCheckSVG,
   IconDeleteSVG,
-  IconEmptySVG,
   LogoSVG,
 };
