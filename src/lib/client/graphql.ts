87aa7504ebb5f09ffe8d6b1d47c00d1926468558
Prince Muel <vansomecsam@gmail.com>
2023-03-26 13:19:55 +0100

    feat: add graphql client
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/lib/client/graphql.ts
@@ -0,0 +1,22 @@
+import { constants } from "@src/helpers";
+import { QueryClientConfig } from "@tanstack/react-query";
+import { GraphQLClient } from "graphql-request";
+
+
+const GRAPHQL_ENDPOINT = `${constants.BASE_URL}/graphql`;
+
+export const client = new GraphQLClient(GRAPHQL_ENDPOINT, {
+  credentials: "include",
+  mode: "cors",
+});
+
+export const queryOptions: QueryClientConfig = {
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnReconnect: true,
+      // refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+};
