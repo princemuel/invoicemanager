88b2652151135023cae3526012671d8f5c83603a
Prince Muel <vansomecsam@gmail.com>
2023-03-26 11:56:12 +0100

    fix: update svg import paths
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/common/svg-icons.ts
+++ src/common/svg-icons.ts
@@ -1,11 +1,11 @@
-import IconArrowLeftSVG from 'assets/svgs/icon-arrow-left.svg';
-import IconArrowRightSVG from 'assets/svgs/icon-arrow-right.svg';
-import IconCalendarSVG from 'assets/svgs/icon-calendar.svg';
-import IconCheckSVG from 'assets/svgs/icon-check.svg';
-import IconDeleteSVG from 'assets/svgs/icon-delete.svg';
-import IconAddSVG from 'assets/svgs/icon-plus.svg';
-import LogoSVG from 'assets/svgs/logo.svg';
-import IconArrowDownSVG from '../assets/svgs/icon-arrow-down.svg';
+import IconArrowDownSVG from '@src/assets/svgs/icon-arrow-down.svg';
+import IconArrowLeftSVG from '@src/assets/svgs/icon-arrow-left.svg';
+import IconArrowRightSVG from '@src/assets/svgs/icon-arrow-right.svg';
+import IconCalendarSVG from '@src/assets/svgs/icon-calendar.svg';
+import IconCheckSVG from '@src/assets/svgs/icon-check.svg';
+import IconDeleteSVG from '@src/assets/svgs/icon-delete.svg';
+import IconAddSVG from '@src/assets/svgs/icon-plus.svg';
+import LogoSVG from '@src/assets/svgs/logo.svg';
 
 export {
   IconAddSVG,
