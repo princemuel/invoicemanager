5fa4887b825770ad271ee7d2b25c06e8f65f0d91
Prince Muel <vansomecsam@gmail.com>
2023-03-26 13:28:10 +0100

    refactor: rename main.css to index.css
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import '@src/assets/styles/main.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 import { ModalProvider, ThemeProvider } from './context';
+import './index.css';
 import { queryOptions } from './lib/client';
 import { router } from './routes';
 
