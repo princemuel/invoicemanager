43e5684aef9831631dcf3499a33e74aa0e0e1726
Prince Muel <vansomecsam@gmail.com>
2023-03-29 07:37:52 +0100

    feat(dev): add query keys for react query
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/lib/client/query-keys.ts
@@ -0,0 +1,35 @@
+import { createQueryKeyStore } from '@lukemorales/query-key-factory';
+import { fetchSingleInvoice } from '../static';
+
+// if you prefer to declare everything in one file
+export const queryKeys = createQueryKeyStore({
+  users: {
+    all: null,
+    detail: (userId: string) => ({
+      queryKey: [userId],
+      // queryFn: () => api.getUser(userId),
+    }),
+  },
+  invoices: {
+    detail: (invoiceId: string) => ({
+      queryKey: [invoiceId],
+      queryFn: () => fetchSingleInvoice(invoiceId),
+    }),
+    // list: (filters: InvoiceFilters) => ({
+    //   queryKey: [{ filters }],
+    //   queryFn: (ctx) => api.getInvoices({ filters, page: ctx.pageParam }),
+    //   contextQueries: {
+    //     search: (query: string, limit = 15) => ({
+    //       queryKey: [query, limit],
+    //       queryFn: (ctx) =>
+    //         api.getSearchInvoices({
+    //           page: ctx.pageParam,
+    //           filters,
+    //           limit,
+    //           query,
+    //         }),
+    //     }),
+    //   },
+    // }),
+  },
+});
