253458c5fe6aa71bde7c46d4c3401400bd59348a
Prince Muel <vansomecsam@gmail.com>
2023-03-30 03:36:54 +0100

    feat: add error handling function
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/helpers/error-handler.ts
+++ src/helpers/error-handler.ts
@@ -1,6 +1,7 @@
 type ErrorWithMessage = {
   message: string;
 };
+type ErrorDataWithMessage = { data: { message: string } };
 
 export function getErrorMessage(error: unknown) {
   return toErrorWithMessage(error).message;
@@ -8,6 +9,7 @@ export function getErrorMessage(error: unknown) {
 
 function toErrorWithMessage(error: unknown): ErrorWithMessage {
   if (isErrorWithMessage(error)) return error;
+  if (isErrorDataMessage(error)) return error.data;
 
   try {
     return new Error(JSON.stringify(error));
@@ -17,11 +19,24 @@ function toErrorWithMessage(error: unknown): ErrorWithMessage {
     return new Error(String(error));
   }
 }
+
+/**
+ * Type predicate to narrow an unknown error to an object with a string 'message' property
+ */
 function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
   return (
-    typeof error === "object" &&
+    typeof error === 'object' &&
     error !== null &&
-    "message" in error &&
-    typeof (error as Record<string, unknown>).message === "string"
+    'message' in error &&
+    typeof (error as Record<string, unknown>).message === 'string'
+  );
+}
+
+function isErrorDataMessage(error: unknown): error is ErrorDataWithMessage {
+  return (
+    typeof error === 'object' &&
+    error != null &&
+    'data' in error &&
+    typeof (error as any).data?.message === 'string'
   );
 }
