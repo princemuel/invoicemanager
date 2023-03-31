d05152de2fe12a44f4f8c91b997b7e0a35cbae69
Prince Muel <vansomecsam@gmail.com>
2023-03-31 22:21:53 +0100

    feat: add calendar class
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/helpers/calendar.ts
+++ src/helpers/calendar.ts
@@ -1,18 +1,67 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 
-export class Calendar {}
+type DateType = Date | string;
 
-export const calendar = new Calendar();
+type FormatDateFunction = (
+  date?: DateType,
+  formatOptions?: Intl.DateTimeFormatOptions[],
+  separator?: string
+) => string;
+
+export class Calendar {
+  private static convert(datetime: DateType) {
+    return new Date(datetime);
+  }
+
+  public static formatDate: FormatDateFunction = (
+    date,
+    formatOptions,
+    separator = ' '
+  ) => {
+    const safeDate = date || new Date().toISOString();
+    formatOptions ||= [
+      { day: 'numeric' },
+      { month: 'short' },
+      { year: 'numeric' },
+    ];
+    return formatOptions
+      .map((options) => {
+        const dateFormatter = new Intl.DateTimeFormat('en', options);
+        return dateFormatter.format(new Date(safeDate));
+      })
+      .join(separator);
+  };
+
+  public static days(datetime: DateType) {
+    const date = 0;
+    const monthIndex = Calendar.convert(datetime).getUTCMonth() + 1;
+    const year = Calendar.year(datetime);
+    return new Date(year, monthIndex, date).getDate();
+  }
+
+  public static date(datetime: DateType) {
+    return Calendar.convert(datetime).getUTCDate();
+  }
+
+  public static month(datetime: DateType) {
+    const number = Calendar.convert(datetime).getUTCMonth();
+    return months[number].slice(0, 3);
+  }
+
+  public static year(datetime: DateType) {
+    return Calendar.convert(datetime).getUTCFullYear();
+  }
+}
