aa1cd195a0551320b0032e2ff46f4910e1713c6e
Prince Muel <vansomecsam@gmail.com>
2023-03-31 22:21:24 +0100

    feat: scaffold register form
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/organisms/register.tsx
+++ src/components/organisms/register.tsx
@@ -4,26 +4,37 @@ const RegisterForm = (props: Props) => {
   return (
     <form>
       <div>
-        <label htmlFor='firstName'>First Name</label>
-        <input id='firstName' type='text' placeholder='Email Address' />
-      </div>
-      <div>
-        <label htmlFor='lastName'>Last Name</label>
-        <input id='lastName' type='text' placeholder='' />
-      </div>
-      <div>
-        <label htmlFor='email'>Email Address</label>
-        <input id='email' type='email' placeholder='' />
+        <label>
+          <input name='firstName' type='text' placeholder='First Name' />
+        </label>
       </div>
 
       <div>
-        <label htmlFor='password'>Password</label>
-        <input id='password' type='password' placeholder='Email Address' />
+        <label>
+          <input name='lastName' type='text' placeholder='Last Name' />
+        </label>
       </div>
 
       <div>
-        <label htmlFor='countersign'>Repeat Password</label>
-        <input id='countersign' type='password' placeholder='Email Address' />
+        <label>
+          <input name='email' type='email' placeholder='Email Address' />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <input name='password' type='password' placeholder='Password' />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          <input
+            name='countersign'
+            type='password'
+            placeholder='Repeat Password'
+          />
+        </label>
       </div>
     </form>
   );
