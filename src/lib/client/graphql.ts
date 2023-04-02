eed9538d08466ca33c70e82923012234aebe82e3
Prince Muel <vansomecsam@gmail.com>
2023-04-02 14:38:05 +0100

    feat: add graphql client options
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/client/graphql.ts
+++ src/lib/client/graphql.ts
@@ -1,5 +1,4 @@
 import { constants } from '@src/helpers';
-import { QueryClientConfig } from '@tanstack/react-query';
 import { GraphQLClient } from 'graphql-request';
 
 const GRAPHQL_ENDPOINT = `${constants.BASE_URL}/graphql`;
@@ -8,14 +7,3 @@ export const client = new GraphQLClient(GRAPHQL_ENDPOINT, {
   credentials: 'include',
   mode: 'cors',
 });
-
-export const queryOptions: QueryClientConfig = {
-  defaultOptions: {
-    queries: {
-      refetchOnMount: false,
-      refetchOnReconnect: true,
-      // refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
-    },
-  },
-};
