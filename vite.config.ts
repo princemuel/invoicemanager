a5fe61e238a006f9052830eda6c62ddc2e46ebe0
Prince Muel <vansomecsam@gmail.com>
2023-04-02 16:31:31 +0100

    ci: update vite configuration
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react(), EnvironmentPlugin('all', { prefix: 'REACT_APP_' })],
   resolve: {
     alias: [{ find: '@src', replacement: '/src/' }],
+  },
+  server: {
+    port: 3000,
+    open: true,
   },
 });
