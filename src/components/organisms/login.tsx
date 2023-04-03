f2ad6201fc727853cce6f2891aa8b3a6d5a02196
Prince Muel <vansomecsam@gmail.com>
2023-04-03 21:06:37 +0100

    feat: scaffold login, register components
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/organisms/login.tsx
+++ src/components/organisms/login.tsx
@@ -2,81 +2,169 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { z } from 'zod';
+// import clsx from 'clsx';
 import { Text } from '../atoms';
 
 type Props = {};
 
-const schema = z.object({
-  email: z.string().email('Invalid email').min(1, 'Email is required'),
-  password: z
-    .string()
-    .min(1, 'Password is required')
-    .min(8, 'Password must have more than 8 characters'),
-});
+const FormSchema = z
+  .object({
+    email: z
+      .string()
+      .email({ message: 'Invalid email address' })
+      .min(1, { message: "Can't be empty" })
+      .min(6, { message: 'Must be or more characters long' })
+      .toLowerCase()
+      .trim(),
+    password: z
+      .string()
+      .min(1, "Can't be empty")
+      .min(8, { message: 'Must be 8 or more characters long' })
+      .trim(),
+    countersign: z.string().min(1, "Can't be empty").trim(),
+  })
+  .refine((data) => data.password === data.countersign, {
+    path: ['countersign'],
+    message: 'The passwords do not match',
+  });
 
-type FormSchema = z.infer<typeof schema>;
+type FormSchemaType = z.infer<typeof FormSchema>;
 
 const LoginForm = (props: Props) => {
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<FormSchema>({
-    resolver: zodResolver(schema),
+    reset,
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
   });
 
-  const onSubmit: SubmitHandler<FormSchema> = (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<FormSchemaType> = async (data) => {
+    await new Promise(async (resolve) => {
+      setTimeout(() => {
+        console.log(data);
+        resolve(undefined);
+      }, 2000);
+      reset();
+    });
   };
+
+  if (isSubmitting) console.log('SUBMITTING DATA');
 
   return (
     <form
-      className='flex w-full max-w-[40rem] flex-col gap-6 rounded-3xl px-12 py-16 dark:bg-brand-700 max-md:mt-48'
+      className='w-full max-w-[40rem] rounded-3xl bg-neutral-100 px-12 py-16 shadow-100 dark:bg-brand-600 max-md:mt-24'
       onSubmit={handleSubmit(onSubmit)}
     >
-      <Text as='h1' className='text-5xl font-normal tracking-300'>
-        Login
-      </Text>
+      <fieldset className='grid grid-cols-6 gap-8'>
+        <Text as='legend' className='text-5xl font-normal tracking-300'>
+          Register
+        </Text>
 
-      <div className=''>
-        <label className='block'>
+        <div className='col-span-6 mt-10 flex flex-col-reverse gap-4'>
           <input
-            type='email'
             {...register('email')}
-            placeholder='Email Address'
-            className='peer w-full border-b border-solid border-brand-100/10 p-6 text-500 text-neutral-100 caret-accent-200 placeholder:text-neutral-100/50 focus:border-neutral-100  focus:outline-none'
+            id='email'
+            type='email'
+            placeholder='Enter email address'
+            className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
+            aria-invalid={errors.email ? 'true' : 'false'}
+            aria-errormessage={'error-email'}
             autoComplete='username'
           />
-        </label>
-      </div>
 
-      <div>
-        <label className='block'>
+          <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+            <label htmlFor='email' className='body-100'>
+              Email
+            </label>
+
+            <Text
+              as='span'
+              role='alert'
+              className='text-200 font-semibold leading-200 tracking-[-0.21px]'
+              id='error-email'
+              aria-live='assertive'
+            >
+              {errors?.email?.message}
+            </Text>
+          </div>
+        </div>
+
+        <div className='col-span-6 flex flex-col-reverse gap-4'>
           <input
             {...register('password')}
             type='password'
-            placeholder='Password'
-            className='peer w-full border-b border-solid border-brand-100/10 bg-inherit p-6 text-500 text-neutral-100 caret-accent-200 placeholder:text-neutral-100/50 focus:border-neutral-100  focus:outline-none'
-            autoComplete='current-password'
+            id='password'
+            placeholder='Enter password'
+            className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
+            aria-invalid={errors.password ? 'true' : 'false'}
+            aria-errormessage={'error-password'}
+            autoComplete='new-password'
           />
-        </label>
-      </div>
 
-      <div>
-        <button
-          type='submit'
-          className='flex w-full items-center justify-center rounded-brand bg-accent-200 p-7 text-500 font-normal'
-        >
-          Login to your account
-        </button>
-      </div>
+          <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+            <label htmlFor='password' className='body-100'>
+              Password
+            </label>
+            <Text
+              as='span'
+              role='alert'
+              id='error-password'
+              className='text-200 font-semibold leading-200 tracking-[-0.21px]'
+              aria-live='assertive'
+            >
+              {errors?.password?.message}
+            </Text>
+          </div>
+        </div>
 
-      <div className='flex items-center justify-center gap-4 text-500'>
-        <Text>Don’t have an account?</Text>
-        <Link to='/register' className='text-accent-200'>
-          Sign Up
-        </Link>
-      </div>
+        <div className='col-span-6 flex flex-col-reverse gap-4'>
+          <input
+            {...register('countersign')}
+            id='countersign'
+            type='password'
+            placeholder='Repeat password'
+            className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
+            aria-invalid={errors.countersign ? 'true' : 'false'}
+            aria-errormessage={'error-countersign'}
+            autoComplete='new-password'
+          />
+
+          <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+            <label htmlFor='countersign' className='body-100'>
+              Confirm Password
+            </label>
+
+            <Text
+              as='span'
+              role='alert'
+              id='error-countersign'
+              className='text-200 font-semibold leading-200 tracking-[-0.21px]'
+              aria-live='assertive'
+            >
+              {errors?.countersign?.message}
+            </Text>
+          </div>
+        </div>
+
+        <div className='col-span-6 mt-6'>
+          <button
+            type='submit'
+            // disabled={isSubmitting}
+            className='btn w-full border-none bg-accent-200 p-6 text-500 font-normal text-neutral-100 outline-none transition duration-500 focus:bg-accent-100 hover:bg-accent-100'
+          >
+            Login to your account
+          </button>
+        </div>
+
+        <div className='col-span-6 flex items-center justify-center gap-4 text-500'>
+          <Text>Don’t have an account?</Text>
+          <Link to='/register' className='text-accent-200'>
+            Sign Up
+          </Link>
+        </div>
+      </fieldset>
     </form>
   );
 };
--- src/components/organisms/register.tsx
+++ src/components/organisms/register.tsx
@@ -1,30 +1,44 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { z } from 'zod';
 // import clsx from 'clsx';
+import { useRegisterMutation } from '@src/hooks';
+import { client } from '@src/lib';
 import { Text } from '../atoms';
 
 type Props = {};
 
 const FormSchema = z
   .object({
+    firstName: z
+      .string()
+      .min(1, { message: "Can't be empty" })
+      .min(2, { message: 'Must be more than 2 characters' })
+      .trim(),
+    lastName: z
+      .string()
+      .min(1, { message: "Can't be empty" })
+      .min(2, { message: 'Must be more than 2 characters' })
+      .trim(),
     email: z
       .string()
-      .min(1, 'Email is required')
-      .email('Invalid email')
+      .email({ message: 'Invalid email address' })
+      .min(1, { message: "Can't be empty" })
+      .min(6, { message: 'Must more than 6 characters' })
       .toLowerCase()
       .trim(),
     password: z
       .string()
-      .min(1, 'Password is required')
-      .min(8, 'Password must have more than 8 characters')
+      .min(1, "Can't be empty")
+      .min(8, 'Must be more than 8 characters')
+      .max(32, 'Must be less than 32 characters')
       .trim(),
-    countersign: z.string().min(1, 'Password confirmation is required').trim(),
+    countersign: z.string().min(1, "Can't be empty").trim(),
   })
   .refine((data) => data.password === data.countersign, {
     path: ['countersign'],
-    message: 'Passwords do not match',
+    message: 'The passwords do not match',
   });
 
 type FormSchemaType = z.infer<typeof FormSchema>;
@@ -38,121 +52,183 @@ const RegisterForm = (props: Props) => {
   } = useForm<FormSchemaType>({
     resolver: zodResolver(FormSchema),
   });
+  const navigate = useNavigate();
+
+  const { data, mutate: createUser } = useRegisterMutation(client, {
+    onSuccess(data) {
+      console.log(data.register?.user);
+      console.log(data.register?.accessToken);
+      // navigate('/login');
+    },
+  });
 
   const onSubmit: SubmitHandler<FormSchemaType> = async (data) => {
-    await new Promise(async (resolve) => {
-      setTimeout(() => {
-        console.log(data);
-        resolve(undefined);
-      }, 2000);
+    console.log(data);
+
+    const { firstName, lastName, email, password } = data;
+
+    if (firstName && lastName && email && password) {
+      createUser({ input: { firstName, lastName, email, password } });
       reset();
-    });
+    }
   };
 
   if (isSubmitting) console.log('SUBMITTING DATA');
 
   return (
     <form
-      className='w-full max-w-[40rem] rounded-3xl bg-neutral-100 px-12 py-16 shadow-100 dark:bg-brand-600 max-md:mt-48'
+      className='w-full max-w-[40rem] rounded-3xl bg-neutral-100 px-12 py-16 shadow-100 dark:bg-brand-600 max-md:mt-24'
       onSubmit={handleSubmit(onSubmit)}
     >
       <fieldset className='grid grid-cols-6 gap-8'>
-        <Text variant='legend' className='text-5xl font-normal tracking-300'>
+        <Text as='legend' className='text-5xl font-normal tracking-300'>
           Register
         </Text>
 
-        <div className='relative col-span-6 mt-10'>
+        <div className='col-span-6 mt-10 flex flex-col-reverse gap-4'>
+          <input
+            {...register('firstName')}
+            id='firstName'
+            type='text'
+            className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
+            aria-invalid={errors.firstName ? 'true' : 'false'}
+            aria-errormessage={'error-firstName'}
+            autoComplete='given-name'
+          />
+
+          <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+            <label htmlFor='firstName' className='body-100'>
+              First Name
+            </label>
+
+            <Text
+              as='span'
+              role='alert'
+              className='text-200 font-semibold leading-200 tracking-[-0.21px]'
+              id='error-firstName'
+              aria-live='assertive'
+            >
+              {errors?.firstName?.message}
+            </Text>
+          </div>
+        </div>
+
+        <div className='col-span-6 flex flex-col-reverse gap-4'>
+          <input
+            {...register('lastName')}
+            id='lastName'
+            type='text'
+            className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
+            aria-invalid={errors.lastName ? 'true' : 'false'}
+            aria-errormessage={'error-lastName'}
+            autoComplete='family-name'
+          />
+
+          <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+            <label htmlFor='lastName' className='body-100'>
+              Last Name
+            </label>
+
+            <Text
+              as='span'
+              role='alert'
+              className='text-200 font-semibold leading-200 tracking-[-0.21px]'
+              id='error-lastName'
+              aria-live='assertive'
+            >
+              {errors?.lastName?.message}
+            </Text>
+          </div>
+        </div>
+
+        <div className='col-span-6 flex flex-col-reverse gap-4'>
           <input
             {...register('email')}
             id='email'
             type='email'
-            placeholder='Enter email address'
-            className='body-100 peer mt-10 w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200  hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:focus:border-brand-500 dark:hover:border-brand-500'
+            className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
             aria-invalid={errors.email ? 'true' : 'false'}
             aria-errormessage={'error-email'}
             autoComplete='username'
           />
 
-          <label
-            htmlFor='email'
-            className='body-100 absolute left-0 top-0 text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'
-          >
-            Email
-          </label>
+          <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+            <label htmlFor='email' className='body-100'>
+              Email
+            </label>
 
-          <Text
-            role='alert'
-            className='absolute right-0 top-0 text-200 font-semibold leading-200 tracking-[-0.21px] peer-aria-[invalid="true"]:text-accent-200'
-            id='error-email'
-            aria-live='assertive'
-          >
-            {errors?.email?.message}
-          </Text>
+            <Text
+              as='span'
+              role='alert'
+              className='text-200 font-semibold leading-200 tracking-[-0.21px]'
+              id='error-email'
+              aria-live='assertive'
+            >
+              {errors?.email?.message}
+            </Text>
+          </div>
         </div>
 
-        <div className='relative col-span-6'>
+        <div className='col-span-6 flex flex-col-reverse gap-4'>
           <input
             {...register('password')}
             type='password'
             id='password'
-            placeholder='Enter password'
-            className='body-100 peer mt-10 w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200  hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:focus:border-brand-500 dark:hover:border-brand-500'
+            className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
             aria-invalid={errors.password ? 'true' : 'false'}
             aria-errormessage={'error-password'}
             autoComplete='new-password'
           />
 
-          <label
-            htmlFor='password'
-            className='body-100 absolute left-0 top-0 text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'
-          >
-            Password
-          </label>
-
-          <Text
-            role='alert'
-            id='error-password'
-            className='absolute right-0 top-0 text-200 font-semibold leading-200 tracking-[-0.21px] peer-aria-[invalid="true"]:text-accent-200'
-            aria-live='assertive'
-          >
-            {errors?.password?.message}
-          </Text>
+          <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+            <label htmlFor='password' className='body-100'>
+              Password
+            </label>
+            <Text
+              as='span'
+              role='alert'
+              id='error-password'
+              className='text-200 font-semibold leading-200 tracking-[-0.21px]'
+              aria-live='assertive'
+            >
+              {errors?.password?.message}
+            </Text>
+          </div>
         </div>
 
-        <div className='relative col-span-6'>
+        <div className='col-span-6 flex flex-col-reverse gap-4'>
           <input
             {...register('countersign')}
             id='countersign'
             type='password'
-            placeholder='Repeat password'
-            className='body-100 peer mt-10 w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200  hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:focus:border-brand-500 dark:hover:border-brand-500'
+            className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
             aria-invalid={errors.countersign ? 'true' : 'false'}
             aria-errormessage={'error-countersign'}
             autoComplete='new-password'
           />
 
-          <label
-            htmlFor='countersign'
-            className='body-100 absolute left-0 top-0 text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'
-          >
-            Confirm Password
-          </label>
+          <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+            <label htmlFor='countersign' className='body-100'>
+              Confirm Password
+            </label>
 
-          <Text
-            role='alert'
-            id='error-countersign'
-            className='absolute right-0 top-0 text-200 font-semibold leading-200 tracking-[-0.21px] peer-aria-[invalid="true"]:text-accent-200'
-            aria-live='assertive'
-          >
-            {errors?.countersign?.message}
-          </Text>
+            <Text
+              as='span'
+              role='alert'
+              id='error-countersign'
+              className='text-200 font-semibold leading-200 tracking-[-0.21px]'
+              aria-live='assertive'
+            >
+              {errors?.countersign?.message}
+            </Text>
+          </div>
         </div>
 
-        <div className='col-span-6'>
+        <div className='col-span-6 mt-6'>
           <button
             type='submit'
             // disabled={isSubmitting}
-            className='flex w-full items-center justify-center rounded-brand bg-accent-200 p-5 text-500 font-normal text-neutral-100'
+            className='btn w-full border-none bg-accent-200 p-6 text-500 font-normal text-neutral-100 outline-none transition duration-500 focus:bg-neutral-100 focus:text-brand-500 hover:bg-neutral-100 hover:text-brand-500'
           >
             Create an account
           </button>
