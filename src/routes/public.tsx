ab04800f3d1e23700fcfb4e2d41010db0e339251
Prince Muel <vansomecsam@gmail.com>
2023-04-06 21:17:31 +0100

    feat: scaffold user login
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,6 +1,10 @@
 // import { formatDate } from '@src/helpers';
-
-import { Calendar } from '@src/helpers';
+import { IUser } from '@src/@types';
+import { useAuthDispatch } from '@src/context';
+import { Calendar, getErrorMessage } from '@src/helpers';
+import { useGetUserQuery, useRefreshAuthQuery } from '@src/hooks';
+import { client } from '@src/lib';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {};
 
@@ -8,7 +12,52 @@ const PublicRoute = (props: Props) => {
   const datetime = new Date('2023-03-31T02:54:50.051Z');
 
   console.log(Calendar.formatDate());
-  return <h1>PublicRoute</h1>;
+  const dispatch = useAuthDispatch();
+  const navigate = useNavigate();
+
+  const { data, refetch, status, error } = useGetUserQuery(
+    client,
+    {},
+    {
+      onSuccess(data) {
+        dispatch({
+          type: 'SET_USER',
+          payload: {
+            user: data.user as IUser,
+          },
+        });
+      },
+      onError(error: any) {
+        error.response.errors.forEach(async (err: any) => {
+          if ((err.message as string).includes('Not Authorised')) {
+            try {
+              const fetcher = useRefreshAuthQuery.fetcher(client);
+              await fetcher();
+              refetch();
+            } catch (error) {
+              navigate('/login');
+            }
+          }
+        });
+      },
+    }
+  );
+
+  if (status === 'loading') {
+    return <h1>Loading...</h1>;
+  }
+
+  if (status === 'error') {
+    return <h1>Error: {JSON.stringify(getErrorMessage(error))}</h1>;
+  }
+
+  const user = data.user;
+
+  return (
+    <div>
+      <h1>{`Welcome ${user?.firstName + ' ' + user?.lastName || 'User'}`}</h1>
+    </div>
+  );
 };
 
 export { PublicRoute };
