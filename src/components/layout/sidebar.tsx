fddf54131db535869843efbe0489d0dbef3d2558
Prince Muel <vansomecsam@gmail.com>
2023-04-06 20:13:15 +0100

    feat: update profile pic img src
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,8 +1,11 @@
+import { useAuthState } from '@src/context';
 import { Logo, ProfilePicture, ThemeButton } from '../atoms';
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
+  const { user } = useAuthState();
+
   return (
     <aside className='sticky top-0 z-50 flex h-32 flex-row items-center justify-between bg-brand-600 max-sx:max-w-full md:min-h-screen md:w-36 md:flex-col md:rounded-tr-[2rem]'>
       <Logo />
@@ -12,7 +15,9 @@ const Sidebar = (props: Props) => {
       </div>
 
       <div className='grid h-full w-32 place-content-center border-l border-[#494E6E] md:h-32 md:w-full md:border-0 md:border-t'>
-        <ProfilePicture imgSrc='/assets/images/image-avatar.jpg' />
+        <ProfilePicture
+          src={user?.photo || '/assets/images/image-avatar.jpg'}
+        />
       </div>
     </aside>
   );
