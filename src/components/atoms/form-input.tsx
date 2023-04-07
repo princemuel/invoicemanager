2362a92e79a386c228c2a62f091f274c684f8821
Prince Muel <vansomecsam@gmail.com>
2023-04-07 05:30:47 +0100

    feat: add form input component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/atoms/form-input.tsx
+++ src/components/atoms/form-input.tsx
@@ -1,23 +1,31 @@
-import type { Props } from '@src/@types';
 import clsx from 'clsx';
-import * as React from 'react';
+import { useFormContext } from 'react-hook-form';
 
-const FormInputInner = <E extends React.ElementType = 'input'>(
-  { as, className, ...rest }: Props<E>,
-  ref: React.ForwardedRef<HTMLInputElement>
-) => {
-  const RenderedElement = as || 'input';
+interface Props
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  name: string;
+}
+
+const FormInput = ({ name, className, ...rest }: Props) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
   return (
-    <RenderedElement
+    <input
+      {...register(name)}
       className={clsx(
         'body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500',
-        '',
         className
       )}
+      aria-invalid={errors?.[name] ? 'true' : 'false'}
+      aria-errormessage={`errors-${name}`}
       {...rest}
-      ref={ref}
     />
   );
 };
-
-export const FormInput = React.forwardRef(FormInputInner);
+export { FormInput };
