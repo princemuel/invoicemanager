a4c82e2c2fca950eab10a39642327e76f9559a03
Prince Muel <vansomecsam@gmail.com>
2023-04-07 05:31:51 +0100

    feat: add form text input component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/molecules/text-input.tsx
+++ src/components/molecules/text-input.tsx
@@ -1,12 +1,48 @@
-import { FormInput } from '../atoms';
+import { useFormContext } from 'react-hook-form';
+import { FormControl, FormErrorText, FormInput, FormLabel } from '../atoms';
 
-type Props = {};
+interface Props
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  name: string;
+  label: string;
+}
 
-const TextInput = (props: Props) => {
+const TextInput = ({
+  name,
+  type,
+  placeholder,
+  label,
+  autoComplete,
+  className,
+}: Props) => {
+  const {
+    formState: { errors },
+  } = useFormContext();
+
   return (
-    <div>
-      <FormInput children={undefined} />
-    </div>
+    <FormControl className={className}>
+      <FormInput
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        className=''
+        autoComplete={autoComplete}
+      />
+
+      <div className='flex items-center justify-between text-brand-400 peer-aria-[invalid="true"]:!text-accent-200 dark:text-brand-300'>
+        <FormLabel htmlFor={name} className=''>
+          {label}
+        </FormLabel>
+
+        <FormErrorText id={name} className=''>{`${
+          errors?.[name]?.message || ''
+        }`}</FormErrorText>
+      </div>
+    </FormControl>
   );
 };
 
