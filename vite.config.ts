83bbb3996fd7c5f336a3cf291483882709464ed2
Prince Muel <vansomecsam@gmail.com>
2023-04-11 17:21:33 +0100

    ci: add vite-plugin-svgr plugin to vite config
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@src', replacement: '/src/' }],
   },
