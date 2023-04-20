abf931d376be3b7f91f162dde674f3080ac575a0
Prince Muel <vansomecsam@gmail.com>
2023-04-20 20:49:59 +0100

    feat: update form error text
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/atoms/form-error-msg.tsx
+++ src/components/atoms/form-error-msg.tsx
@@ -1,24 +1,37 @@
 import clsx from 'clsx';
+import { useFormContext } from 'react-hook-form';
 
 interface Props
   extends React.DetailedHTMLProps<
     React.HTMLAttributes<HTMLSpanElement>,
     HTMLSpanElement
-  > {}
+  > {
+  message?: string;
+}
 
-const FormErrorText = ({ className, children, id, ...rest }: Props) => {
+const FormErrorText = ({
+  id = '',
+  message,
+  className,
+  children,
+  ...rest
+}: Props) => {
+  const {
+    formState: { errors },
+  } = useFormContext();
+
   return (
     <span
       role='alert'
+      id={`errors-${id}`}
+      aria-live='assertive'
       className={clsx(
         'text-200 font-semibold leading-200 tracking-[-0.21px]',
         className
       )}
-      id={`errors-${id}`}
-      aria-live='assertive'
       {...rest}
     >
-      {children}
+      {children || message || `${errors?.[id]?.message || ''}`}
     </span>
   );
 };
