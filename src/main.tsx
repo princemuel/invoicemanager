0b1e6bc41e7e23d93115f7eeba094249905cb998
Prince Muel <vansomecsam@gmail.com>
2023-04-21 05:11:23 +0100

    chore: cleanup main.tsx
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,16 @@
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
-import { RouterProvider } from 'react-router-dom';
-import { AuthProvider, ModalProvider, ThemeProvider } from './context';
 import './index.css';
-// import { queryOptions } from './lib/client';
-import { router } from './routes';
+import { Providers } from './providers';
 
 if (import.meta.env.MODE === 'production') disableReactDevTools();
 
-// const client = new QueryClient(queryOptions);
-const client = new QueryClient();
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <HelmetProvider>
-        <ThemeProvider>
-          <AuthProvider>
-            <ModalProvider>
-              <ReactQueryDevtools
-                position='bottom-right'
-                initialIsOpen={false}
-              />
-              <RouterProvider router={router} />
-            </ModalProvider>
-          </AuthProvider>
-        </ThemeProvider>
-      </HelmetProvider>
-    </QueryClientProvider>
+    <Providers />
   </React.StrictMode>
 );
