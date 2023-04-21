ff3323ef708b95ca498de4650e7cecbd0fa0332a
Prince Muel <vansomecsam@gmail.com>
2023-04-21 23:56:31 +0100

    feat: update page routes
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,60 +1,17 @@
-import { IUser } from '@src/@types';
-import { useAuthDispatch } from '@src/context';
-import { getErrorMessage } from '@src/helpers';
-import { useGetUserQuery, useRefreshAuthQuery } from '@src/hooks';
-import { client } from '@src/lib';
+import { useAuthState } from '@src/context';
 import { useNavigate } from 'react-router-dom';
 
 type Props = {};
 
 const PublicRoute = (props: Props) => {
-  const datetime = new Date('2023-03-31T02:54:50.051Z');
-
-  const dispatch = useAuthDispatch();
   const navigate = useNavigate();
+  const auth = useAuthState();
 
-  const { data, refetch, status, error } = useGetUserQuery(
-    client,
-    {},
-    {
-      onSuccess(data) {
-        dispatch({
-          type: 'SET_USER',
-          payload: {
-            user: data.user as IUser,
-          },
-        });
-      },
-      onError(error: any) {
-        error.response.errors.forEach(async (err: any) => {
-          if ((err.message as string).includes('Not Authorised')) {
-            try {
-              const fetcher = useRefreshAuthQuery.fetcher(client);
-              await fetcher();
-              refetch();
-            } catch (error) {
-              console.log(error);
-              navigate('/login');
-            }
-          }
-        });
-      },
-    }
-  );
-
-  if (status === 'loading') {
-    return <h1>Loading...</h1>;
-  }
-
-  if (status === 'error') {
-    return <h1>Error: {JSON.stringify(getErrorMessage(error))}</h1>;
-  }
-
-  const user = data.user;
+  const user = auth?.user;
 
   return (
     <div>
-      <h1>{`Welcome ${user?.email || 'User'}`}</h1>
+      <h1>{`Welcome User with ${user?.email}`}</h1>
     </div>
   );
 };
