a59933baf5ec20cca544953c440018952586bf61
Prince Muel <vansomecsam@gmail.com>
2023-04-27 04:37:43 +0100

    feat: update invoice template
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/templates/invoice.tsx
+++ src/components/templates/invoice.tsx
@@ -1,3 +1,4 @@
+import { Dialog, Transition } from '@headlessui/react';
 import {
   InvoiceType,
   useDeleteInvoiceMutation,
@@ -7,7 +8,7 @@ import {
 } from '@src/hooks';
 import { client } from '@src/lib';
 import { useQueryClient } from '@tanstack/react-query';
-import { Fragment, useCallback } from 'react';
+import { Fragment, useCallback, useRef, useState } from 'react';
 import {
   Navigate,
   useLocation,
@@ -16,7 +17,7 @@ import {
 } from 'react-router-dom';
 import { InvoiceDetails, InvoiceDetailsMobile } from '../organisms';
 
-type Props = {};
+interface Props {}
 
 const InvoiceTemplate = (props: Props) => {
   const { invoiceId } = useParams();
@@ -25,9 +26,11 @@ const InvoiceTemplate = (props: Props) => {
   if (!invoiceId) {
     return <Navigate to='/' state={{ from: location }} replace />;
   }
-
   const navigate = useNavigate();
   const queryClient = useQueryClient();
+
+  const [isOpen, setIsOpen] = useState(false);
+  let deleteButtonRef = useRef<HTMLButtonElement>(null);
 
   const { data } = useGetInvoiceQuery(client, {
     where: { id: invoiceId },
@@ -82,18 +85,106 @@ const InvoiceTemplate = (props: Props) => {
     removeInvoice({ where: { id: invoiceId } });
   }, []);
 
+  const openModal = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
   return (
     <Fragment>
       <InvoiceDetailsMobile
         invoice={invoice}
         updateStatus={updateStatus}
-        deleteInvoice={deleteInvoice}
+        openDeleteModal={openModal}
       />
       <InvoiceDetails
         invoice={invoice}
         updateStatus={updateStatus}
-        deleteInvoice={deleteInvoice}
+        openDeleteModal={openModal}
       />
+      {/* <Details
+        invoice={invoice}
+        updateStatus={updateStatus}
+        openDeleteModal={openModal}
+      /> */}
+
+      <Transition show={isOpen} as={Fragment}>
+        <Dialog
+          as='div'
+          className='relative z-50'
+          initialFocus={deleteButtonRef}
+          onClose={closeModal}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-brand-900/25' aria-hidden='true' />
+          </Transition.Child>
+
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-center justify-center p-[2.4rem]'>
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel
+                  as='form'
+                  className='mx-auto w-full max-w-3xl rounded-brand bg-neutral-100 shadow-100 dark:bg-brand-700'
+                  onSubmit={() => {
+                    deleteInvoice(invoice);
+                  }}
+                >
+                  <section className='flex w-full flex-col gap-8 p-16'>
+                    <Dialog.Title
+                      as='h2'
+                      className='text-700 font-bold leading-[3.2rem] tracking-[-0.42px]'
+                    >
+                      Confirm Deletion
+                    </Dialog.Title>
+
+                    <Dialog.Description className='body-100 text-brand-300 dark:text-brand-100'>
+                      <span>Are you sure you want to delete invoice</span>{' '}
+                      <span className='uppercase'>#{invoice?.tag}?</span>{' '}
+                      <span> This action cannot be undone.</span>
+                    </Dialog.Description>
+
+                    <div className='flex items-center gap-8'>
+                      <button
+                        type='button'
+                        onClick={closeModal}
+                        className='btn btn-edit ml-auto font-bold'
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type='submit'
+                        ref={deleteButtonRef}
+                        className='btn btn-delete font-bold'
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </section>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </Fragment>
   );
 };
