ccc3683f91d5391cba09a954c24f21bb3c2c7929
Prince Muel <vansomecsam@gmail.com>
2023-04-30 12:54:07 +0100

    chore: refactor invoice details component
    References: #51, #48
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/organisms/invoice-details.tsx
+++ src/components/organisms/invoice-details.tsx
@@ -1,8 +1,8 @@
-import { calculateTotal, formatDate, formatPrice } from '@src/helpers';
+import { datetime } from '@src/helpers';
 import { InvoiceType, useMedia } from '@src/hooks';
 import * as React from 'react';
 import { BackLink, StatusButton, Text } from '../atoms';
-import { InvoiceActions } from '../molecules';
+import { InvoiceActions, InvoiceItemDetails } from '../molecules';
 
 interface Props {
   invoice?: InvoiceType;
@@ -11,159 +11,145 @@ interface Props {
 }
 
 const InvoiceDetails = ({ invoice, updateStatus, openDeleteModal }: Props) => {
-  const isWide = useMedia(`(min-width: 50em)`);
-
+  const isMobile = useMedia(`(max-width: 50em)`);
   return (
     <React.Fragment>
-      <section className='h-container max-md:hidden'>
-        <BackLink url='/invoices' className='mt-20' />
+      <section aria-label=''>
+        <div className='h-container'>
+          <BackLink url='/invoices' className='mt-20' />
+        </div>
 
-        <article className='mb-20 mt-10 grid gap-12'>
-          <header className='flex items-center justify-between rounded-brand bg-neutral-100 px-10 py-8 shadow-100 dark:bg-brand-700'>
-            <div className='flex items-center justify-between gap-8'>
-              <Text className='body-100 text-[#858BB2] dark:text-brand-100'>
-                Status
-              </Text>
-              {/* @ts-expect-error */}
-              <StatusButton status={invoice?.status} className='px-14 py-6' />
-            </div>
-
-            <InvoiceActions
-              updateStatus={updateStatus}
-              openDeleteModal={openDeleteModal}
-            />
-          </header>
-
-          <div className='flex flex-col gap-12 rounded-brand bg-neutral-100 px-20 py-12 shadow-100 dark:bg-brand-700'>
-            <div className='flex justify-between'>
-              <div className='> * + * space-y-4'>
-                <Text className='text-600 font-bold leading-500 tracking-400'>
-                  <span className='text-brand-400'>#</span>
-                  <span className='uppercase text-brand-900 dark:text-neutral-100'>
-                    {invoice?.tag}
-                  </span>
+        <div className='h-container'>
+          <article className='mb-20 mt-10 grid gap-12'>
+            <header className='flex items-center justify-between rounded-brand bg-neutral-100 px-10 py-8 shadow-100 dark:bg-brand-700'>
+              <div className='flex w-full items-center gap-8'>
+                <Text className='body-100 text-[#858BB2] dark:text-brand-100'>
+                  Status
                 </Text>
+                <StatusButton
+                  // @ts-expect-error
+                  status={invoice?.status}
+                  className='ml-auto px-14 py-6 md:ml-0 md:mr-auto'
+                />
 
-                <Text className='body-100 text-brand-400 dark:text-brand-100'>
-                  {invoice?.description}
-                </Text>
+                {!isMobile ? (
+                  <InvoiceActions
+                    invoice={invoice}
+                    updateStatus={updateStatus}
+                    openDeleteModal={openDeleteModal}
+                    className=''
+                  />
+                ) : (
+                  <React.Fragment />
+                )}
               </div>
+            </header>
 
-              <div>
-                <Text className='body-200 text-brand-400 dark:text-brand-100 '>
-                  {invoice?.senderAddress?.street}
-                </Text>
-                <Text className='body-200 text-brand-400 dark:text-brand-100 '>
-                  {invoice?.senderAddress?.city}
-                </Text>
-                <Text className='body-200 text-brand-400 dark:text-brand-100 '>
-                  {invoice?.senderAddress?.postCode}
-                </Text>
-                <Text className='body-200 text-brand-400 dark:text-brand-100'>
-                  {invoice?.senderAddress?.country}
-                </Text>
-              </div>
-            </div>
-
-            <div className='flex justify-between gap-20'>
-              <div className='flex flex-1 flex-col gap-12'>
+            <div className='flex flex-col gap-20 rounded-brand bg-neutral-100 px-10 py-12 shadow-100 dark:bg-brand-700 md:px-20'>
+              <div className='flex flex-col justify-between gap-12 sx:flex-row'>
                 <div className='> * + * space-y-4'>
-                  <Text className='body-100 text-brand-400 dark:text-brand-100'>
-                    Invoice Date
+                  <Text className='text-600 font-bold leading-500 tracking-400'>
+                    <span className='text-brand-400'>#</span>
+                    <span className='uppercase text-brand-900 dark:text-neutral-100'>
+                      {invoice?.tag}
+                    </span>
                   </Text>
-                  <Text className='body-300'>
-                    {formatDate(invoice?.updatedAt)}
-                  </Text>
-                </div>
 
-                <div className='> * + * space-y-4'>
                   <Text className='body-100 text-brand-400 dark:text-brand-100'>
-                    Payment Due
+                    {invoice?.description}
                   </Text>
-                  <Text className='body-300'>
-                    {formatDate(invoice?.paymentDue)}
-                  </Text>
-                </div>
-              </div>
-
-              <div className='> * + * flex-1 space-y-5'>
-                <div className='> * + * space-y-4'>
-                  <Text className='body-100 text-brand-400 dark:text-brand-100'>
-                    Bill To
-                  </Text>
-                  <Text className='body-300'>{invoice?.clientName}</Text>
                 </div>
 
                 <address>
-                  <Text className='body-100 text-brand-400 dark:text-brand-100'>
-                    {invoice?.clientAddress?.street}
+                  <Text className='body-200 text-brand-400 dark:text-brand-100'>
+                    {invoice?.senderAddress?.street}
                   </Text>
-                  <Text className='body-100 text-brand-400 dark:text-brand-100'>
-                    {invoice?.clientAddress?.city}
+                  <Text className='body-200 text-brand-400 dark:text-brand-100'>
+                    {invoice?.senderAddress?.city}
                   </Text>
-                  <Text className='body-100 text-brand-400 dark:text-brand-100'>
-                    {invoice?.clientAddress?.postCode}
+                  <Text className='body-200 text-brand-400 dark:text-brand-100'>
+                    {invoice?.senderAddress?.postCode}
                   </Text>
-                  <Text className='body-100 text-brand-400 dark:text-brand-100'>
-                    {invoice?.clientAddress?.country}
+                  <Text className='body-200 text-brand-400 dark:text-brand-100'>
+                    {invoice?.senderAddress?.country}
                   </Text>
                 </address>
               </div>
 
-              <div className='> * + * flex-[2] space-y-4'>
-                <Text className='body-100 text-brand-400 dark:text-brand-100 '>
-                  Sent to
-                </Text>
-                <Text className='body-300'>{invoice?.clientEmail}</Text>
+              <div className='flex justify-between gap-x-24 gap-y-16 max-md:flex-wrap'>
+                <div className='flex flex-initial flex-col gap-12'>
+                  <div className='> * + * space-y-4'>
+                    <Text className='body-100 text-brand-400 dark:text-brand-100'>
+                      Invoice Date
+                    </Text>
+                    <Text className='body-300'>
+                      {datetime.toDateString(invoice?.updatedAt)}
+                    </Text>
+                  </div>
+
+                  <div className='> * + * space-y-4'>
+                    <Text className='body-100 text-brand-400 dark:text-brand-100'>
+                      Payment Due
+                    </Text>
+                    <Text className='body-300'>
+                      {datetime.toDateString(invoice?.paymentDue)}
+                    </Text>
+                  </div>
+                </div>
+
+                <div className='mr-auto flex flex-initial flex-col gap-5'>
+                  <div className='> * + * space-y-4'>
+                    <Text className='body-100 text-brand-400 dark:text-brand-100'>
+                      Bill To
+                    </Text>
+                    <Text className='body-300 truncate'>
+                      {invoice?.clientName}
+                    </Text>
+                  </div>
+
+                  <address>
+                    <Text className='body-100 text-brand-400 dark:text-brand-100'>
+                      {invoice?.clientAddress?.street}
+                    </Text>
+                    <Text className='body-100 text-brand-400 dark:text-brand-100'>
+                      {invoice?.clientAddress?.city}
+                    </Text>
+                    <Text className='body-100 text-brand-400 dark:text-brand-100'>
+                      {invoice?.clientAddress?.postCode}
+                    </Text>
+                    <Text className='body-100 text-brand-400 dark:text-brand-100'>
+                      {invoice?.clientAddress?.country}
+                    </Text>
+                  </address>
+                </div>
+
+                <div className='> * + * flex-1 space-y-4'>
+                  <Text className='body-100 text-brand-400 dark:text-brand-100'>
+                    Sent to
+                  </Text>
+                  <Text className='body-300 truncate'>
+                    {invoice?.clientEmail}
+                  </Text>
+                </div>
               </div>
+
+              <InvoiceItemDetails items={invoice?.items} />
             </div>
+          </article>
+        </div>
 
-            <table className='grid auto-cols-min grid-cols-1 gap-8 overflow-clip rounded-brand pt-10 shadow-100 dark:bg-brand-600'>
-              <thead className='px-14'>
-                <tr className='grid grid-cols-4 justify-items-end text-brand-400 dark:text-brand-100'>
-                  <th className='body-200 justify-self-start'>Item Name</th>
-                  <th className='body-200 '>QTY.</th>
-                  <th className='body-200 '>Price</th>
-                  <th className='body-200 '>Total</th>
-                </tr>
-              </thead>
-
-              <tbody className='flex flex-col gap-10 px-14 py-4'>
-                {invoice?.items?.map((item) => {
-                  return (
-                    <tr
-                      //!NOTE: change this to item id later
-                      key={item.name}
-                      className='grid grid-cols-4 justify-items-end gap-4'
-                    >
-                      <td className='body-100 justify-self-start font-bold text-brand-900 dark:text-neutral-100'>
-                        {item?.name}
-                      </td>
-                      <td className='body-100 font-bold text-brand-400 dark:text-neutral-100'>
-                        {item?.quantity}
-                      </td>
-                      <td className='body-100 font-bold text-brand-400 dark:text-neutral-100'>
-                        {formatPrice(item?.price)}
-                      </td>
-                      <td className='body-100 font-bold text-brand-900 dark:text-neutral-100'>
-                        {formatPrice(item?.total)}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-
-              <tfoot className='w-full-shadow bg-[#373B53] px-14 py-10 text-brand-900 dark:bg-brand-900'>
-                <tr className='flex items-center justify-between '>
-                  <th className='body-200 text-neutral-100'>Amount Due</th>
-                  <td className='text-700 font-bold leading-[3.2rem] tracking-[-0.63px] text-neutral-100 '>
-                    {formatPrice(calculateTotal(invoice?.items))}
-                  </td>
-                </tr>
-              </tfoot>
-            </table>
+        {isMobile ? (
+          <div className='px-[2.4rem] py-9 dark:bg-brand-700'>
+            <InvoiceActions
+              invoice={invoice}
+              updateStatus={updateStatus}
+              openDeleteModal={openDeleteModal}
+              className=''
+            />
           </div>
-        </article>
+        ) : (
+          <React.Fragment />
+        )}
       </section>
     </React.Fragment>
   );
