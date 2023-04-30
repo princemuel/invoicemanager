47a0641c78e15b8cc923db91688a7e34913ca3c3
Prince Muel <vansomecsam@gmail.com>
2023-04-30 12:53:15 +0100

    feat: update form input component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/atoms/form-input.tsx
+++ src/components/atoms/form-input.tsx
@@ -9,7 +9,7 @@ interface Props
   name: string;
 }
 
-const FormInput = ({ name, className, ...rest }: Props) => {
+const FormInput = ({ name, className, type, ...rest }: Props) => {
   const {
     register,
     formState: { errors },
@@ -17,11 +17,14 @@ const FormInput = ({ name, className, ...rest }: Props) => {
 
   return (
     <input
-      {...register(`${name}`)}
+      {...register(`${name}`, {
+        valueAsNumber: type === 'number' || undefined,
+      })}
       className={clsx(
         'body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500',
         className
       )}
+      type={type}
       aria-invalid={errors?.[name] ? 'true' : 'false'}
       aria-errormessage={`errors-${name}`}
       {...rest}
