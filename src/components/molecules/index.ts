098a711d2904ceda563caeb6a20a0a3479ffc0e4
Prince Muel <vansomecsam@gmail.com>
2023-04-30 12:57:33 +0100

    feat: add new invoice item list component
    References: #51
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -3,4 +3,5 @@ export * from './calendar';
 export * from './dropdown';
 export * from './form-field';
 export * from './invoice-item-details';
+export * from './new-item-list';
 export * from './query-result';
