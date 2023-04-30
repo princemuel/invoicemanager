1d3b4f8eaf7eabcc4ac928f9eb2d2c6c4e563b0a
Prince Muel <vansomecsam@gmail.com>
2023-04-30 17:44:31 +0100

    chore: cleanup unnecessary code
    References: #48, #51
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/components/organisms/edit-invoice.tsx
+++ src/components/organisms/edit-invoice.tsx
@@ -4,7 +4,6 @@ import {
   RHFSubmitHandler,
   calculateTotal,
   constants,
-  formatPrice,
   hasValues,
   terms,
   useZodForm,
@@ -17,8 +16,8 @@ import {
 import { client } from '@src/lib';
 import { useQueryClient } from '@tanstack/react-query';
 import produce from 'immer';
-import { useEffect, useReducer, useState } from 'react';
-import { FormProvider, useFieldArray } from 'react-hook-form';
+import { useReducer, useState } from 'react';
+import { FormProvider } from 'react-hook-form';
 import {
   Navigate,
   useLocation,
@@ -26,8 +25,8 @@ import {
   useParams,
 } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
-import { FormErrorText, Text } from '../atoms';
-import { Calendar, Dropdown, FormField } from '../molecules';
+import { Text } from '../atoms';
+import { Calendar, Dropdown, EditItemList, FormField } from '../molecules';
 
 interface Props {}
 
@@ -63,6 +62,8 @@ const EditInvoiceForm = (props: Props) => {
 
   const invoice = data?.invoice;
 
+  console.table(invoice);
+
   const [isShowing, setIsShowing] = useReducer((prev) => !prev, false);
   const [status, setStatus] = useState(invoice?.status);
   const [selectedTerm, setSelectedTerm] = useState(
@@ -74,7 +75,7 @@ const EditInvoiceForm = (props: Props) => {
 
   const methods = useZodForm({
     schema: InvoiceFormSchema,
-    mode: 'onChange',
+    mode: 'all',
     defaultValues: {
       clientAddress: {
         street: invoice?.clientAddress?.street || '',
@@ -99,18 +100,6 @@ const EditInvoiceForm = (props: Props) => {
       total: invoice?.total || 0,
     },
   });
-  const { fields, append, remove } = useFieldArray({
-    name: 'items',
-    control: methods.control,
-    rules: {
-      required: 'Please add at least one item',
-    },
-  });
-
-  useEffect(() => {
-    // @ts-expect-error
-    methods.setValue('items', invoice?.items);
-  }, [invoice?.items]);
 
   const onSubmit: RHFSubmitHandler<typeof InvoiceFormSchema> = async (data) => {
     if (!invoiceId) throw new ReferenceError('The invoice id is not defined');
@@ -128,9 +117,8 @@ const EditInvoiceForm = (props: Props) => {
 
         for (const item of draft?.items) {
           item.id = uuid();
-          item.total = calculateTotal(item?.quantity || 0, item?.price || 0);
         }
-        draft.total = calculateTotal(draft?.items || 0);
+        draft.total = calculateTotal(draft?.items, 'total');
       });
 
       const result = InvoiceFormSchema.safeParse(draft);
@@ -320,123 +308,8 @@ const EditInvoiceForm = (props: Props) => {
               Item List
             </legend>
 
-            <ul className='mt-6 grid grid-cols-8 gap-x-6 gap-y-10 sx:grid-cols-12'>
-              <li className='body-100 col-span-8 text-brand-400 dark:text-brand-100 sx:col-span-5'>
-                Item Name
-              </li>
-              <li className='body-100 col-span-2 text-brand-400 dark:text-brand-100'>
-                Qty.
-              </li>
-              <li className='body-100 col-span-3 text-brand-400 dark:text-brand-100 sx:col-span-2'>
-                Price
-              </li>
-              <li className='body-100 col-span-2 text-brand-400 dark:text-brand-100'>
-                Total
-              </li>
-            </ul>
-
-            <ul className='mt-6 flex flex-col gap-8'>
-              {fields.map((field, index) => {
-                const errors = methods.formState.errors?.items?.[index];
-
-                return (
-                  <li
-                    key={field.id}
-                    className='grid grid-cols-8 items-end gap-x-6 gap-y-10 sx:grid-cols-12'
-                  >
-                    <label className='col-span-8 flex flex-col-reverse gap-2 sx:col-span-5'>
-                      <input
-                        type='text'
-                        {...methods.register(`items.${index}.name`)}
-                        id={`items.${index}.name`}
-                        className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
-                        aria-invalid={errors?.name ? 'true' : 'false'}
-                        aria-errormessage={`errors-items.${index}.name`}
-                        defaultValue={invoice?.items?.[index]?.name || ''}
-                      />
-
-                      <FormErrorText
-                        id={`items.${index}.name`}
-                        className='text-right peer-aria-[invalid="true"]:!text-accent-200'
-                      >
-                        {errors?.name?.message || ''}
-                      </FormErrorText>
-                    </label>
-
-                    <label className='col-span-2'>
-                      <input
-                        type='number'
-                        {...methods.register(`items.${index}.quantity`, {
-                          valueAsNumber: true,
-                        })}
-                        id={`items.${index}.quantity`}
-                        className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
-                        step={1}
-                        aria-invalid={errors?.quantity ? 'true' : 'false'}
-                        defaultValue={invoice?.items?.[index]?.quantity || 0}
-                      />
-                    </label>
-
-                    <label className='col-span-3 sx:col-span-2'>
-                      <input
-                        type='number'
-                        {...methods.register(`items.${index}.price`, {
-                          valueAsNumber: true,
-                        })}
-                        id={`items.${index}.price`}
-                        className='body-100 peer w-full rounded-lg border border-brand-100 bg-neutral-100 px-8 py-6 font-bold text-brand-900 caret-brand-500 outline-none autofill:bg-neutral-100 focus:border-brand-500 aria-[invalid="true"]:!border-accent-200 aria-[invalid="true"]:!text-accent-200 focus:aria-[invalid="true"]:!border-accent-200 focus:aria-[invalid="true"]:!ring-accent-200 hover:border-brand-500 dark:border-brand-600 dark:bg-brand-700 dark:text-neutral-100 dark:autofill:bg-brand-700 dark:focus:border-brand-500 dark:hover:border-brand-500'
-                        aria-invalid={errors?.price ? 'true' : 'false'}
-                        defaultValue={invoice?.items?.[index]?.price || 0}
-                        step={0.01}
-                      />
-                    </label>
-
-                    <div className='col-span-2 self-center'>
-                      <output
-                        htmlFor={`items.${index}.price items.${index}.quantity`}
-                        id={`items.${index}.total`}
-                        className='body-100 block font-bold text-[#888EB0]'
-                      >
-                        {formatPrice(
-                          calculateTotal(
-                            field?.quantity || 0,
-                            field?.price || 0
-                          )
-                        )}
-                      </output>
-                    </div>
-
-                    <div className='col-span-1 self-center'>
-                      <button
-                        type='button'
-                        className='inline-block h-[1.6rem] w-[1.3rem] bg-[url(/assets/svgs/icon-delete.svg)] bg-cover bg-no-repeat focus:bg-[url(/assets/svgs/icon-delete-red.svg)] hover:bg-[url(/assets/svgs/icon-delete-red.svg)]'
-                        onClick={() => void remove(index)}
-                      >
-                        <span className='sr-only'>Delete Item</span>
-                      </button>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-
-            <div className='mt-16'>
-              <button
-                type='button'
-                className='btn btn-add'
-                onClick={() =>
-                  void append({
-                    id: uuid(),
-                    name: '',
-                    quantity: 0,
-                    price: 0,
-                    total: 0,
-                  })
-                }
-              >
-                &#43; Add New Item
-              </button>
-            </div>
+            {/* @ts-expect-error */}
+            <EditItemList methods={methods} invoice={invoice} />
           </fieldset>
           {/*<!--------- ITEM DETAILS END ---------!>*/}
         </div>
