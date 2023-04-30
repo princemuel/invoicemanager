2c23fc2f5059c5d69a2a2febcee40c9571c1b225
Prince Muel <vansomecsam@gmail.com>
2023-04-30 17:37:05 +0100

    feat: add price output component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/components/molecules/price-output.tsx
@@ -0,0 +1,45 @@
+import clsx from 'clsx';
+import {
+  FieldError,
+  FieldErrorsImpl,
+  Merge,
+  useFormContext,
+} from 'react-hook-form';
+
+interface Props {
+  index: number;
+  errors:
+    | Merge<
+        FieldError,
+        FieldErrorsImpl<{
+          name: string;
+          quantity: number;
+          price: number;
+          id: string;
+          total: number;
+        }>
+      >
+    | undefined;
+}
+
+const PriceOutput = ({ index, errors }: Props) => {
+  const { register } = useFormContext();
+
+  return (
+    <input
+      type='number'
+      {...register(`items.${index}.total`, {
+        valueAsNumber: true,
+      })}
+      id={`items.${index}.total`}
+      className={clsx(
+        'body-100 w-full appearance-none bg-inherit font-bold text-[#888EB0] outline-none',
+        errors ? 'mt-10 sx:mt-[2.3rem]' : 'mt-9 sx:mt-3'
+      )}
+      step={0.01}
+      readOnly
+    />
+  );
+};
+
+export { PriceOutput };
