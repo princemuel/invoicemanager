7e55e898a42f5076bfa33454cdd9bb560055ba82
Prince Muel <vansomecsam@gmail.com>
2023-05-06 04:49:26 +0100

    chore: cleanup barrel exports files
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
-export * from './react-query';
 export * from './use-form';
 export * from './use-media';
 export * from './use-persist';
+export * from './use-react-query';
 export * from './use-session';
