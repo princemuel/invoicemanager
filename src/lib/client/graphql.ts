9ef2d535fed4693568705cce5abc2162c52efce4
Prince Muel <vansomecsam@gmail.com>
2023-05-08 16:53:52 +0100

    feat: update graphql endpoint
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/client/graphql.ts
+++ src/lib/client/graphql.ts
@@ -1,7 +1,7 @@
 import { constants } from '@src/helpers';
 import { GraphQLClient } from 'graphql-request';
 
-const endpoint = `${constants.BASE_URL}/api/graphql`; //  process?.env?.NEXT_PUBLIC_GRAPHQL_ENDPOINT ??
+const endpoint = `${constants.BASE_URL}`; //  process?.env?.NEXT_PUBLIC_GRAPHQL_ENDPOINT ??
 
 export const client = new GraphQLClient(endpoint, {
   credentials: 'include',
