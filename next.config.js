79e2faa9f4fead9c045fd144594be311d7502ea2
Prince Muel <vansomecsam@gmail.com>
2023-06-11 02:42:30 +0100

    ci: update vscode, tailwind, next.js config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+    webVitalsAttribution: ['CLS', 'LCP'],
+    // turbo: {
+    //   loaders: { '.svg': ['@svgr/webpack'] },
+    // },
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ['@svgr/webpack'],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
