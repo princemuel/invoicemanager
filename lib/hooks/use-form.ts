7e7d08c001a52aebc23d3b718e0500e1c49206ba
Prince Muel <vansomecsam@gmail.com>
2023-06-12 19:13:09 +0100

    feat: add react hooks
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ lib/hooks/use-form.ts
@@ -0,0 +1,33 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as React from 'react';
+import { UseFormProps, useForm as useReactHookForm } from 'react-hook-form';
+import { z } from 'zod';
+
+export function useZodForm<T extends z.ZodType>(
+  props: Omit<UseFormProps<T['_input']>, 'resolver'> & {
+    schema: T;
+  }
+) {
+  return useReactHookForm<T['_input']>({
+    ...props,
+    resolver: zodResolver(props.schema, undefined, {
+      // This makes it so we can use `.transform()`s on the schema without same transform getting applied again when it reaches the server
+      raw: true,
+    }),
+  });
+}
+
+export function useForm<State extends Record<string, unknown>>(
+  initialState: State
+) {
+  const [values, setValues] = React.useState(() => initialState);
+
+  const update = React.useCallback(
+    <Key extends keyof State>(name: Key, value: State[Key]) => {
+      setValues((fields) => ({ ...fields, [name]: value }));
+    },
+    []
+  );
+
+  return [values, update] as const;
+}
