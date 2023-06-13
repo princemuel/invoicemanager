3d28ab5d10b99a4d614dd8fc34d76054b535b45a
Prince Muel <vansomecsam@gmail.com>
2023-06-13 11:12:35 +0100

    fix: resolve issue with tailwind config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class', '[data-mode="dark"]'],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     borderRadius: {
       ...defaultTheme.borderRadius,
