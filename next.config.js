3e6da420dc0deda64fa2cddb3f755b1245d73845
Prince Muel <vansomecsam@gmail.com>
2023-06-16 16:10:47 +0100

    ci: add image config to nextjs config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next.config.js
+++ next.config.js
@@ -31,6 +31,9 @@ const nextConfig = {
     fileLoaderRule.exclude = /\.svg$/i;
     return config;
   },
+  images: {
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+  },
 };
 
 module.exports = nextConfig;
