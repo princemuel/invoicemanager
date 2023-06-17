cb026789d59b6f0acc7d175bb177963dddf6d921
Prince Muel <vansomecsam@gmail.com>
2023-06-17 06:49:21 +0100

    feat: update prisma client setup
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -4,14 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const db =
-  globalThis.prisma ||
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === 'production'
-        ? ['error']
-        : ['query', 'error', 'warn'],
-  });
+const db = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
 
 export default db;
