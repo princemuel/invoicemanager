32fa2dfb0165e7f573b67d0a810b9afb0f711f09
Prince Muel <vansomecsam@gmail.com>
2023-06-18 01:20:03 +0100

    feat: add invoice item list component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,3 +1,7 @@
 export * from './auth-providers';
 export * from './base-modal';
+export * from './datepicker';
 export * from './form-field';
+export * from './invoice-items-a';
+export * from './price-output';
+export * from './terms';
---
+++ components/molecules/price-output.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { cx } from 'cva';
+import {
+  FieldError,
+  FieldErrorsImpl,
+  Merge,
+  useFormContext,
+} from 'react-hook-form';
+
+interface Props {
+  index: number;
+  errors:
+    | Merge<
+        FieldError,
+        FieldErrorsImpl<{
+          name: string;
+          quantity: number;
+          price: number;
+          id: string;
+          total: number;
+        }>
+      >
+    | undefined;
+}
+
+const PriceOutput = ({ index, errors }: Props) => {
+  const { register } = useFormContext();
+
+  return (
+    <input
+      type='text'
+      {...register(`items.${index}.total`, {
+        valueAsNumber: true,
+      })}
+      id={`items.${index}.total`}
+      className={cx(
+        'body-100 w-full appearance-none bg-inherit font-bold text-[#888EB0] outline-none',
+        errors ? 'mt-10 sx:mt-[2.3rem]' : 'mt-9 sx:mt-3'
+      )}
+      step={0.01}
+      readOnly
+    />
+  );
+};
+
+export { PriceOutput };
