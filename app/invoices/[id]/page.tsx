72bf31f2c01a1aac65ee3c0dc98d59434b92e451
Prince Muel <vansomecsam@gmail.com>
2023-06-19 16:15:28 +0100

    feat: add invoice details page
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/invoices/[id]/page.tsx
+++ app/invoices/[id]/page.tsx
@@ -1,7 +1,16 @@
-async function PageRoute({ params }: { params: IParams }) {
-  console.log(params);
+import { getInvoiceById } from '@/app/lib/get-invoice-by-id';
+import { EmptyState, InvoiceTemplate } from '@/components';
 
-  return <div>PageRoute</div>;
+async function PageRoute({ params }: { params: IParams }) {
+  const invoice = await getInvoiceById(params);
+
+  if (!invoice) return <EmptyState />;
+
+  return (
+    <main className='w-full'>
+      <InvoiceTemplate invoice={invoice} />;
+    </main>
+  );
 }
 
 export default PageRoute;
