05d7dfc593c6928d753aa0c814d7233aafa09bc0
Prince Muel <vansomecsam@gmail.com>
2023-07-02 12:07:11 +0100

    feat: update emptystate component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- components/atoms/empty-state.tsx
+++ components/atoms/empty-state.tsx
@@ -1,27 +1,49 @@
+'use client';
+
+import { useCreateInvoiceModal, useLoginModal } from '@/lib';
 import Image from 'next/image';
 import { Text } from './text';
 
-const EmptyState = () => {
-  return (
-    <article className='flex flex-col items-center gap-20 text-center'>
-      <Image
-        src={'/assets/illustration-empty.svg'}
-        width='242'
-        height='200'
-        className='w-full'
-        alt={'Invoices List Empty'}
-      />
+interface Props {
+  user?: string | null;
+}
 
-      <div className='flex flex-col items-center gap-8 px-16'>
-        <Text as='h2' className=''>
-          There is nothing here
-        </Text>
-        <Text as='p' className='max-w-[22rem]'>
-          <span>Create an invoice by clicking the</span>{' '}
-          <em className='font-bold'>New Invoice</em> button and get started
-        </Text>
-      </div>
-    </article>
+const EmptyState = ({ user }: Props) => {
+  const login = useLoginModal();
+  const invoiceModal = useCreateInvoiceModal();
+
+  return (
+    <section className='flex w-full items-center justify-center'>
+      <article className='flex flex-col items-center gap-20 text-center'>
+        <Image
+          src={'/assets/illustration-empty.svg'}
+          width='242'
+          height='200'
+          className='w-full'
+          alt={'Invoices List Empty'}
+        />
+        <div className='flex flex-col items-center gap-8 px-16'>
+          <Text as='h2' className=''>
+            {user ? 'There is nothing here' : 'You are not logged in'}
+          </Text>
+          <Text as='p' className='max-w-[22rem]'>
+            {user ? (
+              <button type='button' onClick={invoiceModal.open}>
+                Please
+                <em className='font-bold underline underline-offset-2'>
+                  create a new Invoice
+                </em>
+                to get started
+              </button>
+            ) : (
+              <button className='underline' onClick={login.open}>
+                Please login to view your invoices
+              </button>
+            )}
+          </Text>
+        </div>
+      </article>
+    </section>
   );
 };
 
