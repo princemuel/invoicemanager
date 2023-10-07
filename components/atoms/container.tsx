87e34e204c48fe3a70ee90cdfe5a2ed9d37e6d82
Prince Muel <vansomecsam@gmail.com>
2023-10-07 21:50:53 +0100

    feat: add container component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ components/atoms/container.tsx
@@ -0,0 +1,57 @@
+import { cn } from '@/helpers';
+import * as React from 'react';
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {}
+
+const Outer = React.forwardRef<HTMLDivElement, Props>(
+  ({ className, children, ...props }, forwardedRef) => {
+    return (
+      <div ref={forwardedRef} className='sm:px-8' {...props}>
+        <div
+          className={cn('max-w-screen-2xl mx-auto w-full lg:px-8', className)}
+        >
+          {children}
+        </div>
+      </div>
+    );
+  }
+);
+Outer.displayName = 'OuterContainer';
+
+const Inner = React.forwardRef<HTMLDivElement, Props>(
+  ({ className, children, ...props }, forwardedRef) => {
+    return (
+      <div
+        ref={forwardedRef}
+        className='relative px-4 md:px-8 lg:px-12'
+        {...props}
+      >
+        <div
+          className={cn(
+            'mx-auto max-w-screen-md lg:max-w-screen-lg',
+            className
+          )}
+        >
+          {children}
+        </div>
+      </div>
+    );
+  }
+);
+Inner.displayName = 'InnerContainer';
+
+const Wrapper = React.forwardRef<HTMLDivElement, Props>(
+  ({ children, ...props }, forwardedRef) => {
+    return (
+      <Outer ref={forwardedRef} {...props}>
+        <Inner>{children}</Inner>
+      </Outer>
+    );
+  }
+);
+Wrapper.displayName = 'Container';
+
+export const Container = Object.assign({}, Wrapper, {
+  Inner: Inner,
+  Outer: Outer,
+});
