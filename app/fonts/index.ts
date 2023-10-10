19eb0e2c48eaecb2b53e94f0d4f3bf42968127b6
Prince Muel <vansomecsam@gmail.com>
2023-10-10 04:23:12 +0100

    feat: update font assets
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -4,22 +4,29 @@ import localFont from 'next/font/local';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const FontSans_Dev = localFont({
-  src: './league-spartan.ttf',
-  variable: '--font-sans',
+const FontSans = localFont({
   display: 'swap',
+  variable: '--font-sans',
+  src: [
+    {
+      path: './spartan-medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './spartan-bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
 });
+
 const FontAccent_Dev = localFont({
   src: './rubik.ttf',
   variable: '--font-accent',
   display: 'swap',
 });
 
-const FontSans_Prod = League_Spartan({
-  subsets: ['latin'],
-  variable: '--font-sans',
-  display: 'swap',
-});
 const FontAccent_Prod = League_Spartan({
   subsets: ['latin'],
   variable: '--font-sans',
@@ -28,6 +35,6 @@ const FontAccent_Prod = League_Spartan({
 
 export const fonts = cn(
   isProduction
-    ? [FontSans_Prod.variable, FontAccent_Prod.variable]
-    : [FontSans_Dev.variable, FontAccent_Dev.variable]
+    ? [FontSans.variable, FontAccent_Prod.variable]
+    : [FontSans.variable, FontAccent_Dev.variable]
 );
