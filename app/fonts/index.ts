09c39ac1b39a1120c8fd9a74f3c686174e539935
Prince Muel <vansomecsam@gmail.com>
2023-10-10 03:51:47 +0100

    feat: update assets
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/fonts/index.ts
+++ app/fonts/index.ts
@@ -1,16 +1,33 @@
-import { cn } from '@/lib';
+import { cn } from '@/helpers';
+import { League_Spartan } from 'next/font/google';
 import localFont from 'next/font/local';
 
-const FontSans = localFont({
+const isProduction = process.env.NODE_ENV === 'production';
+
+const FontSans_Dev = localFont({
   src: './league-spartan.ttf',
   variable: '--font-sans',
   display: 'swap',
 });
+const FontSans_Prod = League_Spartan({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+});
 
-const FontAccent = localFont({
+const FontAccent_Dev = localFont({
   src: './rubik.ttf',
   variable: '--font-accent',
   display: 'swap',
 });
+const FontAccent_Prod = League_Spartan({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  display: 'swap',
+});
 
-export const fonts = cn(FontAccent.variable, FontSans.variable);
+export const fonts = cn(
+  isProduction
+    ? [FontSans_Prod.variable, FontAccent_Prod]
+    : [FontSans_Dev.variable, FontAccent_Dev]
+);
