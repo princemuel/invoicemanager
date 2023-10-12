7f0706f1eaadef6be09a721b3a1caa6da7291b13
Prince Muel <vansomecsam@gmail.com>
2023-10-12 22:44:28 +0100

    chore: temporarily disable type-check in prod build
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   experimental: {
     typedRoutes: true,
