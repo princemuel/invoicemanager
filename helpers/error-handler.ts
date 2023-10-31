d45cee103ecf1a79f911d6991f331d1fbe41b4c4
Prince Muel <vansomecsam@gmail.com>
2023-10-31 21:34:16 +0100

    feat: update helper functions
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- helpers/error-handler.ts
+++ helpers/error-handler.ts
@@ -1,79 +1,39 @@
-import createHttpError from 'http-errors';
-import { NextResponse } from 'next/server';
 import { z } from 'zod';
+import { HttpError, isHttpError } from 'http-errors-enhanced';
 
-export function handleError<T extends unknown>(error: T) {
+export function handleServerError(exception: unknown) {
   // Errors with statusCode >= 500 are should not be exposed
-  if (createHttpError.isHttpError(error) && error.expose) {
-    // Handle all errors thrown by http-errors module
-    return NextResponse.json(
-      { error: { message: error.message } },
-      { status: error.statusCode }
-    );
-  } else if (error instanceof z.ZodError) {
-    // Handle zod validation errors
-    return NextResponse.json(
+  if (isHttpError(exception)) {
+    const error = exception as HttpError;
+    return error.expose
+      ? Response.json(
+          { status: 'failed', error: error.message },
+          { status: error.status }
+        )
+      : Response.json(
+          {
+            status: 'failed',
+            error: 'An unkwown error occurred. Please try again',
+          },
+          { status: error.status }
+        );
+  } else if (exception instanceof z.ZodError) {
+    return Response.json(
       {
-        error: {
-          message: error.errors.map((error) => error.message).join('; '),
-        },
+        status: 'failed',
+        error: exception.errors.map((issue) => issue.message).join('; '),
       },
       { status: 400 }
     );
-  } else {
-    // default to 500 server error
-    return NextResponse.json({
-      error: { message: 'Internal Server Error', error },
-      status: createHttpError.isHttpError(error) ? error.statusCode : 500,
-    });
   }
+
+  return Response.json(
+    {
+      status: 'failed',
+      error: 'An unkwown error occurred. Please try again',
+    },
+    {
+      status: 500,
+    }
+  );
 }
-
-// result.error.flatten((issue: ZodIssue) => ({
-//   message: issue.message,
-//   errorCode: issue.code,
-// }));
-
-// type FormattedErrors = z.inferFormattedError<typeof FormData>;
-// /*
-//   {
-//     name?: {_errors?: string[]},
-//     contactInfo?: {
-//       _errors?: string[],
-//       email?: {
-//         _errors?: string[],
-//       },
-//       phone?: {
-//         _errors?: string[],
-//       },
-//     },
-//   }
-// */
-
-// type FlattenedErrors = z.inferFlattenedErrors<typeof FormData>;
-// /*
-//   {
-//     formErrors: string[],
-//     fieldErrors: {
-//       email?: string[],
-//       password?: string[],
-//       confirm?: string[]
-//     }
-//   }
-// */
-
-// type FormDataErrors = z.inferFlattenedErrors<
-//   typeof FormData,
-//   { message: string; errorCode: string }
-// >;
-
-// /*
-//   {
-//     formErrors: { message: string, errorCode: string }[],
-//     fieldErrors: {
-//       email?: { message: string, errorCode: string }[],
-//       password?: { message: string, errorCode: string }[],
-//       confirm?: { message: string, errorCode: string }[]
-//     }
-//   }
-// */
--- helpers/index.ts
+++ helpers/index.ts
@@ -1,6 +1,5 @@
 export * from './constants';
 export * from './error-handler';
-export * from './errors';
 export * from './fetcher';
 export * from './forms';
 export * from './utils';
