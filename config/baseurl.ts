2f9cca9f28b30305f9334d5ebd944c9c4a38e2ad
Prince Muel <vansomecsam@gmail.com>
2023-10-31 21:39:14 +0100

    feat: update seo config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- config/baseurl.ts
+++ config/baseurl.ts
@@ -1,12 +1,11 @@
-export const getBaseUrl = () => {
+export const resolveBaseURL = () => {
   let url =
     process.env.SITE_URL ??
     process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    'http://localhost:3000/';
+    `http://localhost:${process.env.PORT || 3000}/`;
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
   // Make sure to including trailing `/`.
-  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-  return url;
+  return url.charAt(url.length - 1) === '/' ? url : `${url}/`;
 };
