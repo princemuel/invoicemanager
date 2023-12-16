6ceb5e10c3a65797adf15a928faff6c353d828c7
Prince Muel <vansomecsam@gmail.com>
2023-12-16 21:06:45 +0100

    feat: setup react svg components for remix
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ app/common/IconCheck.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIconCheck = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={10} height={8} {...props}><path fill="none" stroke="#FFF" strokeWidth={2} d="m1.5 4.5 2.124 2.124L8.97 1.28" /></svg>;
+export default SvgIconCheck;
---
+++ app/common/IconPlus.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIconPlus = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={11} height={11} {...props}><path fill="#7C5DFA" d="M6.313 10.023v-3.71h3.71v-2.58h-3.71V.023h-2.58v3.71H.023v2.58h3.71v3.71z" /></svg>;
+export default SvgIconPlus;
