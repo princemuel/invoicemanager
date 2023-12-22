9558e8469b7c003bd12df161131dc0dc70d07165
Prince Muel <vansomecsam@gmail.com>
2023-12-22 05:39:16 +0100

    ci: update eslint config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
 };
