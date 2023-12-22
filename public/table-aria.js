c9108db4fa04239d415d871abb063e0e10658bb5
Prince Muel <vansomecsam@gmail.com>
2023-12-22 04:05:39 +0100

    style: format files w/ prettier
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- public/table-aria.js
+++ public/table-aria.js
@@ -1,31 +1,31 @@
 (function () {
-  'use strict';
+  "use strict";
 
   try {
-    var allTables = document.querySelectorAll('table');
+    var allTables = document.querySelectorAll("table");
     for (var i = 0; i < allTables.length; i++) {
-      allTables[i].setAttribute('role', 'table');
+      allTables[i].setAttribute("role", "table");
 
-      var allRowGroups = allTables[i].querySelectorAll('thead, tbody, tfoot');
+      var allRowGroups = allTables[i].querySelectorAll("thead, tbody, tfoot");
       for (var j = 0; j < allRowGroups.length; j++) {
-        allRowGroups[j].setAttribute('role', 'rowgroup');
+        allRowGroups[j].setAttribute("role", "rowgroup");
 
-        var allRows = allRowGroups[j].querySelectorAll('tr');
+        var allRows = allRowGroups[j].querySelectorAll("tr");
         for (var k = 0; k < allRows.length; k++) {
-          allRows[k].setAttribute('role', 'row');
+          allRows[k].setAttribute("role", "row");
 
-          var allCells = allRows[k].querySelectorAll('td');
+          var allCells = allRows[k].querySelectorAll("td");
           for (var l = 0; l < allCells.length; l++) {
-            allCells[l].setAttribute('role', 'cell');
+            allCells[l].setAttribute("role", "cell");
           }
-          var allHeaders = allRows[k].querySelectorAll('th');
+          var allHeaders = allRows[k].querySelectorAll("th");
           for (var l = 0; l < allHeaders.length; l++) {
-            if (!allHeaders[l].matches('th[scope=row]')) {
-              allHeaders[l].setAttribute('role', 'columnheader');
+            if (!allHeaders[l].matches("th[scope=row]")) {
+              allHeaders[l].setAttribute("role", "columnheader");
             }
             // this accounts for scoped row headers
             else {
-              allHeaders[l].setAttribute('role', 'rowheader');
+              allHeaders[l].setAttribute("role", "rowheader");
             }
           }
           // caption role not needed as it is not a real role and
@@ -34,6 +34,6 @@
       }
     }
   } catch (e) {
-    console.log('AddTableARIA(): ' + e);
+    console.log("AddTableARIA(): " + e);
   }
 })();
