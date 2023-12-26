d9693fb9e2f0220dd9d3c418a59a8a79d0f01c35
Prince Muel <vansomecsam@gmail.com>
2023-12-26 11:55:58 +0100

    feat: update database setup
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/database/db.server.ts
+++ app/database/db.server.ts
@@ -1,38 +1,12 @@
 import { singleton } from "@/helpers/utils";
 import { PrismaClient } from "@prisma/client";
-import chalk from "chalk";
 
-export const db = singleton("prisma", () => {
-  const THRESHHOLD = 20;
+export const db = singleton("__db__", () => {
   const production = process.env.NODE_ENV === "production";
-
   const client = new PrismaClient({
     errorFormat: production ? "minimal" : "pretty",
-    log:
-      production ?
-        ["error"]
-      : [
-          { level: "query", emit: "event" },
-          { level: "error", emit: "stdout" },
-          { level: "warn", emit: "stdout" },
-        ],
+    log: production ? ["error"] : ["query", "error", "warn"],
   });
-
-  client.$on("query", async (e) => {
-    if (e.duration < THRESHHOLD) return;
-
-    const color =
-      e.duration < THRESHHOLD * 1.1 ? "green"
-      : e.duration < THRESHHOLD * 1.2 ? "blue"
-      : e.duration < THRESHHOLD * 1.3 ? "yellow"
-      : e.duration < THRESHHOLD * 1.4 ? "redBright"
-      : "red";
-
-    const duration = chalk[color](`${e.duration}ms`);
-    console.info(`prisma:query - ${duration} - ${e.query}`);
-  });
-
   client.$connect();
-
   return client;
 });
--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,3 +1,1 @@
-export * from "./use-api-state";
-export * from "./use-fetch-status";
 export * from "./use-hydrated";
