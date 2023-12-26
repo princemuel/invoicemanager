9dc03021e8a5eacfab31b4bdfd5186cd530470ae
Prince Muel <vansomecsam@gmail.com>
2023-12-26 17:05:13 +0100

    refactor: update invoice filters component
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/components/invoice-filters.tsx
+++ app/components/invoice-filters.tsx
@@ -2,6 +2,8 @@ import { IconCheck } from "@/common";
 import { Form, useSearchParams, useSubmit } from "@remix-run/react";
 import { Text } from "./text";
 
+const stats = ["draft", "pending", "paid"];
+
 export function InvoiceFilters() {
   const submit = useSubmit();
 
@@ -14,62 +16,26 @@ export function InvoiceFilters() {
       className="flex w-28 flex-col items-start gap-2 rounded-lg bg-white p-3 sm:w-48 sm:gap-4 sm:p-6 dark:bg-brand-600"
       onChange={(e) => submit(e.currentTarget)}
     >
-      <div className="flex flex-row-reverse items-center gap-3">
-        <Text as="label" htmlFor="draft" weight="bold">
-          Draft
-        </Text>
+      {stats.map((stat) => (
+        <div key={stat} className="flex flex-row-reverse items-center gap-3">
+          <Text as="label" htmlFor={stat} weight="bold" className="capitalize">
+            {stat}
+          </Text>
 
-        <div className="relative size-3.5">
-          <input
-            type="checkbox"
-            id="draft"
-            name="status"
-            value="draft"
-            defaultChecked={statuses.includes("draft")}
-            className="peer absolute top-0 size-full appearance-none rounded-sm border border-brand-400/25 bg-brand-100 checked:bg-brand-500 hover:border-brand-500 dark:bg-brand-700 dark:checked:bg-brand-500"
-          />
+          <div className="relative size-3.5">
+            <input
+              type="checkbox"
+              id={stat}
+              name="status"
+              value={stat}
+              defaultChecked={statuses.includes(stat)}
+              className="peer absolute top-0 size-full cursor-pointer appearance-none rounded-sm border border-brand-400/25 bg-brand-100 checked:bg-brand-500 hover:border-brand-500 dark:bg-brand-700 dark:checked:bg-brand-500"
+            />
 
-          <IconCheck className="pointer-events-none absolute left-1/2 top-1/4 hidden -translate-x-1/2 peer-checked:block" />
+            <IconCheck className="pointer-events-none absolute left-1/2 top-1/4 hidden -translate-x-1/2 peer-checked:block" />
+          </div>
         </div>
-      </div>
-
-      <div className="flex flex-row-reverse items-center gap-3">
-        <Text as="label" htmlFor="pending" weight="bold">
-          Pending
-        </Text>
-
-        <div className="relative size-3.5">
-          <input
-            type="checkbox"
-            id="pending"
-            name="status"
-            value="pending"
-            defaultChecked={statuses.includes("pending")}
-            className="peer absolute top-0 size-full appearance-none rounded-sm border border-brand-400/25 bg-brand-100 checked:bg-brand-500 hover:border-brand-500 dark:bg-brand-700 dark:checked:bg-brand-500"
-          />
-
-          <IconCheck className="pointer-events-none absolute left-1/2 top-1/4 hidden -translate-x-1/2 peer-checked:block" />
-        </div>
-      </div>
-
-      <div className="flex flex-row-reverse items-center gap-3">
-        <Text as="label" htmlFor="paid" weight="bold">
-          Paid
-        </Text>
-
-        <div className="relative size-3.5">
-          <input
-            type="checkbox"
-            id="paid"
-            name="status"
-            value="paid"
-            defaultChecked={statuses.includes("paid")}
-            className="peer absolute top-0 size-full appearance-none rounded-sm border border-brand-400/25 bg-brand-100 checked:bg-brand-500 hover:border-brand-500 dark:bg-brand-700 dark:checked:bg-brand-500"
-          />
-
-          <IconCheck className="pointer-events-none absolute left-1/2 top-1/4 hidden -translate-x-1/2 peer-checked:block" />
-        </div>
-      </div>
+      ))}
     </Form>
   );
 }
