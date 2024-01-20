2a7e246f628ab34fe403da2246d24b8bbe7c88f7
Prince Muel <vansomecsam@gmail.com>
2024-01-20 22:12:38 +0100

    feat: fix add item btn styles
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -69,11 +69,10 @@ export const buttonVariants = cva(
     variants: {
       variant: {
         default: "",
-        primary:
-          "bg-brand-500 text-white hocus:bg-brand-200",
+        primary: "bg-brand-500 text-white hocus:bg-brand-200",
         secondary:
           "bg-accent-300 text-brand-300 hocus:bg-brand-900 dark:bg-accent-300 dark:text-brand-100 dark:hocus:bg-brand-700",
-        soft: "bg-neutral-200 text-brand-400 hocus:bg-brand-100 dark:bg-brand-600 dark:text-brand-100 dark:hocus:bg-white",
+        soft: "bg-neutral-200 text-brand-400 hocus:bg-brand-100 dark:bg-brand-600 dark:text-brand-100 dark:hocus:bg-white dark:hocus:text-brand-400",
         destructive: "bg-accent-200 text-white hocus:bg-accent-100",
       },
       modifier: {
